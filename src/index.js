954ca42311f280445204639244befc8a6bf4c22a
Tyler668 <tyler668@gmail.com>
2021-10-02 16:25:20 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-import App from './App';
-import './sass/index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, withRouter } from "react-router-dom";
+import App from "./App";
+import "./sass/index.scss";
 // import './index.css';
 
 // const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-  <Router>
+  <Router basename={"greatgrate.com"}>
     <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
