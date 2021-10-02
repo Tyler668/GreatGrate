29e38eb4ee915d077098f204cc371ac47e8b8fbc
Tyler668 <tyler668@gmail.com>
2021-10-02 16:10:05 -0400

    deploying

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/GGlogo.png";
 
+// xsds
 const Navigation = () => {
   return (
     <div className="Nav">
